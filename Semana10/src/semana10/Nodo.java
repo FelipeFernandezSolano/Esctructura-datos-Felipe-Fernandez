d617deaaaedd8998bd433877c50a596a42c0c786
FelipeFernandezSolano <lfernandez40990@ufide.ac.cr>
2026-03-19 20:09:26 -0600

    Lec10
    Arboles Binarios

---
+++ Semana10/src/semana10/Nodo.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package semana10;
+
+/**
+ *
+ * @author pipe-
+ */
+public class Nodo {
+    private int dato;
+    private Nodo izquierda;
+    private Nodo derecha;
+    
+    
+    public Nodo (int valor){
+        this.dato = valor;
+        this.derecha = null;
+        this. izquierda = null;
+    }
+
+    public int getDato() {
+        return dato;
+    }
+
+    public void setDato(int dato) {
+        this.dato = dato;
+    }
+
+    public Nodo getIzquierda() {
+        return izquierda;
+    }
+
+    public void setIzquierda(Nodo izquierda) {
+        this.izquierda = izquierda;
+    }
+
+    public Nodo getDerecha() {
+        return derecha;
+    }
+
+    public void setDerecha(Nodo derecha) {
+        this.derecha = derecha;
+    }
+    
+    
+    
+}
